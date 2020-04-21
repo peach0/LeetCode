830d6e67a2362a6d4c5cdb93144812a6e09cb905
peach <peach@peachdeMacBook-Pro.local>
2020-04-21 09:20:06 +0800

    init

--- code/string/string_test.go
+++ code/string/string_test.go
@@ -1,12 +1,14 @@
 package stringCode
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLongestSubstring(t *testing.T) {
 	str := ""
 	t.Log(lengthOfLongestSubstring(str))
 }
-//3. 无重复字符的最长子串
+//3. 重复字符的最长子串
 func lengthOfLongestSubstring(s string) int {
 	m := make(map[byte]int)
 	n := len(s)
@@ -20,7 +22,61 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	return  len
 }
+func TestPalindRome(t *testing.T) {
+	s := "aaaa"
+	print(longestPalindrome(s))
+}
+//5. 最长回文子串
+func longestPalindrome(s string) string {
+	n := len(s)
+	if n <= 1 {
+		return s
+	}
+	start := 0
+	max   := 1
+	dp := make([]bool, n)
 
+	for r := 0; r < n; r++ {
+		dp[r] = true
+		for l := 0; l < r; l++ {
+			if s[l] == s[r] && (r-l  <= 2 || dp[l+1]) {
+				dp[l] = true
+				if r - l + 1 > max {
+					start = l
+					max   = r - l + 1
+				}
+			}else {
+				dp[l] = false
+			}
+		}
+	}
+	return  s[start:start+max]
+}
+
+func TestConvert(t *testing.T) {
+	s := "ABCD"
+	println(convert(s, 3))
+}
+//6. Z 字形变换
+//LCIRETOESIIGEDHN
+//LCIRETOESIIEDHN
+func convert(s string, numRows int) string {
+	if s == "" || len(s) <=  numRows || numRows <= 1 {
+		return s
+	}
+	var b []byte
+	for i := 0; i < numRows ; i ++ {
+		j := i
+		for j< len(s) {
+			b = append(b, s[j])
+			if i != 0 && i != numRows -1 && 2 * numRows + j - 2 * i - 2 < len(s){
+				b = append(b, s[2 * numRows + j - 2 * i - 2])
+			}
+			j = j + 2 * numRows - 2
+		}
+	}
+	return string(b)
+}
 func Max(x, y int) int {
 	if x > y {
 		return x
